23fea74a673afb1963263856e92bd9fbf04be487
uzma shaik <uzma97910@gmail.com>
2025-04-01 23:31:48 +0200

    feat: add team routes

--- app.ts
+++ app.ts
@@ -4,18 +4,22 @@ import { config } from "./config/index";
 import indexRoutes from "./src/routes/index.routes";
 import authRoutes from "./src/routes/auth.routes";
 import { errorHandling } from "./src/error-handling";
+import teamsRoutes from "./src/routes/team.routes";
+import { isAuthenticated } from "./src/middleware/jwt.middleware";
 
 dotenv.config();
 
 const app = express();
 
 config(app);
-errorHandling(app);
 
 app.use("/api", indexRoutes);
 
 app.use("/auth", authRoutes);
 
+app.use("/teams", isAuthenticated, teamsRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+errorHandling(app);
 
 export default app;
--- src/routes/team.routes.ts
+++ src/routes/team.routes.ts
@@ -1,11 +1,34 @@
 import express from "express";
 import { Request, Response, Router } from "express";
-import { isAuthenticated } from "../middleware/jwt.middleware";
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const router = Router();
 
-
-router.post("/teams", async (req, res) => {
+router.get("/", async (req, res) => {
 
 })
+
+router.post("/", async (req: Request, res: Response) => {
+    try {
+        const { teamName, createdBy } = req.body;
+        const newTeam = {
+            teamName,
+            createdBy,
+            createdByEmail: req.user?.email || "unknown",
+        }
+
+        console.log(newTeam);
+        const response = await prisma.team.create({
+            data: newTeam,
+        })
+        res.send(response);
+
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+)
 
 export default router;
