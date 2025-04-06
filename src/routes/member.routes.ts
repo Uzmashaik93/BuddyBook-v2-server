1f2dc32310a3398e561272eb4f14f7f51f83097d
uzma shaik <uzma97910@gmail.com>
2025-04-06 17:30:06 +0200

    feat: add CRUD operations for member management by teamId

--- src/routes/member.routes.ts
+++ src/routes/member.routes.ts
@@ -67,5 +67,87 @@ router.post("/:teamId/member", async (req: Request, res: Response, next: NextFun
 })
 
 
+router.get("/:teamId/member/:memberId", async (req: Request, res: Response, next: NextFunction) => {
+    const { teamId, memberId } = req.params;
+    console.log("teamId", teamId);
+    console.log("memberId", memberId);
+    try {
+        const response = await prisma.member.findUnique({
+            where: {
+                id: memberId
+            },
+            include: {
+                comments: true,
+                customAnswers: true
+            }
+        });
+        if (!response) {
+            res.status(404).json({ message: "Member not found" });
+            return;
+        }
+        res.status(200).json({ message: "Member fetched successfully", member: response })
+    }
+    catch (error) {
+        console.log("Error", error);
+        res.status(500).json(error)
+    }
+})
+
+router.put("/:teamId/member/:memberId", async (req: Request, res: Response, next: NextFunction) => {
+    const { teamId, memberId } = req.params;
+    console.log("teamId", teamId);
+    console.log("memberId", memberId);
+    try {
+        const { age,
+            customQuestion,
+            hobbies,
+            linkedIn,
+            name,
+            place,
+            question1,
+            question2,
+        } = req.body;
+        const response = await prisma.member.update({
+            where: {
+                id: memberId
+            },
+            data: {
+                age,
+                customQuestion,
+                hobbies,
+                linkedIn,
+                name,
+                place,
+                question1,
+                question2,
+                teamId
+            }
+        });
+        res.status(200).json({ message: "Member updated", member: response })
+    }
+    catch (error) {
+        console.log("Error", error);
+        res.status(500).json(error)
+    }
+})
+
+router.delete("/:teamId/member/:memberId", async (req: Request, res: Response, next: NextFunction) => {
+    const { teamId, memberId } = req.params;
+    console.log("teamId", teamId);
+    console.log("memberId", memberId);
+    try {
+        const response = await prisma.member.delete({
+            where: {
+                id: memberId
+            }
+        });
+        res.status(200).json({ message: "Member deleted", member: response })
+    }
+    catch (error) {
+        console.log("Error", error);
+        res.status(500).json(error)
+    }
+})
+
 
 export default router;
